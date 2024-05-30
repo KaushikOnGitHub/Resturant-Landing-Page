d93a6822ba61879085623fd677aca39e47931cc0
Kaushik Solanki <75100949+KaushikOnGitHub@users.noreply.github.com>
2024-05-30 19:30:12 +0530

    resturant landing page

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Home from './Components/Home';
+import About from './Components/About';
+import Work from "./Components/Work";
+import Testimonial from './Components/Testimonials';
+import Contacts from './Components/Contacts';
+import Footer from "./Components/Footer";
+
+
+
+function App() {
+  return (
+    <div className="App"> 
+      <Home/> 
+      <About/>
+      <Work/>
+      <Testimonial/>
+      <Contacts/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
